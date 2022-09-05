b957d4c8f04dc06104fbec5553202349f68b725e
Anton Illenberger <i@gidroonn.ru>
2022-09-05 15:15:19 +0400

    Added an 'about' page and corrected the main page

--- Django_App/Django_App/settings.py
+++ Django_App/Django_App/settings.py
@@ -19,6 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+from os import getenv
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-l25$7c$j2h6=bl%g33v9kzso&j1#21))mw63tha_4gg4$2$h8t'
 
@@ -31,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'main',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
